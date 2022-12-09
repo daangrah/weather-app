8bfc524ed82a69d8ddc07a540605f585f9eed835
Daniil <46220468+daangrah@users.noreply.github.com>
2022-12-09 15:23:23 +0300

    not working

--- weather-app-saga/src/redux/sagas/index.js
+++ weather-app-saga/src/redux/sagas/index.js
@@ -1,24 +1,18 @@
-import { takeEvery, call } from 'redux-saga/effects'
+import { takeEvery, call, all } from 'redux-saga/effects'
 
 const API_key = '2e19406cc850004ee8896c266ef62484';
 
-const fetchWeather = (lat, lon) => {fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`).then(r => r)}
-const fetchPlace = (city_name) => {fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${API_key}`)}
+const fetchWeather = (lat, lon) => {fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`).then(r => r.json())}
+const fetchPlace = (city_name) => {fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${API_key}`).then(r => r.json())}
 
 export function* workerGetPlace() {
   const city_name = prompt('Enter your city');
   const data = yield call(fetchPlace(city_name));
-  const json = yield call(()=> new Promise(res => res(data.json())))
-  const lat = json[0].lat;
-  const lon = json[0].lon;
-  console.log(json)
-  yield getWeatherWorker(lat, lon);
-}
-
-export function* getWeatherWorker(lat, lon){
+  console.log(data)
+  const lat = data[0].lat;
+  const lon = data[0].lon;
   const weather = yield call(fetchWeather(lat, lon));
-  const json = yield call(()=> new Promise(res => res(weather.json())))
-  console.log(json)
+  console.log(weather)
 }
 
 export function* watcherClickSaga () {
@@ -26,5 +20,5 @@ export function* watcherClickSaga () {
 }
 
 export default function* rootSaga() {
-  yield watcherClickSaga();
+  yield all([watcherClickSaga()])
 }
